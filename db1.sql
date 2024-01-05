745911f2b54803b17dcc79274776b0c131787414
minsudex <minsuf4@naver.com>
2024-01-05 11:25:58 +0900

    alter

--- db1.sql
+++ db1.sql
@@ -720,3 +720,49 @@ select sum(o.o_saleprice), c.c_name from customer_table c, orders_table o
 -- 29. 손흥민 고객의 총 구매수량과 고객명을 함께 조회
 select count(o.customer_id), c.c_name from customer_table c, orders_table o 
 	where c.id = o.customer_id and c.c_name = '손흥민';
+-- 30. 가장 비싼 도서의 이름을 조회 
+select b_bookname from book_table where b_price = (select max(b_price) from book_table); 
+-- 31. 책을 구매한 이력이 있는 고객의 이름을 조회
+select c_name from customer_table where id in(1,2,3,4);
+select customer_id from orders_table;
+select c_name from customer_table where id in(select customer_id from orders_table);
+select distinct c.c_name from customer_table c, orders_table o where c.id = o.customer_id;
+-- 32. 도서의 가격(PRICE)과 판매가격(SALEPRICE)의 차이가 가장 많이 나는 주문 조회
+select max(b.b_price - o.o_saleprice) from orders_table o, book_table b where b.id = o.book_id;
+select o.* from orders_table o, book_table b where b.id = o.book_id
+	and b.b_price - o.o_saleprice = 
+		(select max(b.b_price - o.o_saleprice) from orders_table o, book_table b where b.id = o.book_id);
+-- 33. 고객별 평균 구매 금액이 도서의 판매 평균 금액 보다 높은 고객의 이름 조회 
+-- 도서 판매 평균 금액
+select avg(o_saleprice) from orders_table;
+-- 고객별 평균 구매 금액(group by)
+select c.c_name, avg(o.o_saleprice)from customer_table c, orders_table o where c.id = o.customer_id group by c.id ;
+select c.c_name from customer_table c, orders_table o 
+	where c.id = o.customer_id group by c.id 
+		having avg(o.o_saleprice) > (select avg(o_saleprice) from orders_table);
+-- 34. 고객번호가 5인 고객의 주소를 대한민국 인천으로 변경 
+update customer_table set c_address = "대한민국 인천" where id = 5;
+-- 35. 김씨 성을 가진 고객이 주문한 총 판매액 조회
+select c.c_name, sum(o.o_saleprice) from orders_table o, customer_table c 
+	where c.id = o.customer_id and c.c_name like "김%" group by c.c_name;
+select sum(o_saleprice) from orders_table where customer_id in (select id from customer where c_name like '김%');
+
+-- alter: 테이블의 구조를 변경할 때(컬럼이름 변경, 타입 변경, 컬럼삭제, 제약조건 추가 등)
+create table student(
+	id bigint,
+    s_name varchar(20),
+    s_mobile int
+);
+-- 테이블의 구조 확인
+desc student;
+desc book_table;
+-- 기존 컬럼에 제약조건 추가
+alter table student add constraint primary key(id);
+-- 기존 컬럼 타입 변경
+alter table student modify s_mobile varchar(30);
+-- 컬럼 추가
+alter table student add s_major varchar(30);
+-- 컬럼 이름 변경
+alter table student change s_mobile s_phone varchar(30);
+-- 컬럼 삭제
+alter table student drop s_major; 
